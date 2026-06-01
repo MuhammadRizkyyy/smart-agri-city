8bb99f8f1b7ece1dc06a0d87b518ea2b99ad125a
Fadlan Fadilah <fadlanfadilah250@gmail.com>
2026-06-01 14:48:36 +0700

    fix: adjust local database testing credentials

--- php-crop/app/Models/Database.php
+++ php-crop/app/Models/Database.php
@@ -12,7 +12,7 @@ class Database {
             $host = getenv('DB_HOST') ?: '127.0.0.1';
             $db_name = getenv('DB_NAME') ?: 'agriCity';
             $username = getenv('DB_USER') ?: 'root';
-            $password = getenv('DB_PASS') ?: 'rootpass';
+            $password = getenv('DB_PASS') ?: '';
 
             try {
                 $dsn = "mysql:host=" . $host . ";dbname=" . $db_name . ";charset=utf8mb4";
